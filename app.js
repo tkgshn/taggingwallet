8acf970a6890ebfafcf564c6e813634b5b715b3b
tkgshn <takagishunsuke1129@gmail.com>
2022-10-15 10:51:50 -0500

    SQL conection is ready

--- app.js
+++ app.js
@@ -1,169 +1,59 @@
-// const express = require('express');
-// const mysql = require('mysql');
-// const app = express();
-
-// app.use(express.static('public'));
-// app.use(express.urlencoded({extended: false}));
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'progate',
-//   password: '', //これGitHubとかに上げるときは消さなきゃいけない
-//   database: 'list_app'
-// });
-
-// //SQLログイン時のエラーハンドリング
-// connection.connect((err) => {
-//   if (err) {
-//     console.log('error connecting: ' + err.stack);
-//     return;
-//   }
-//   console.log('success');
-// });
-
-// app.get('/', (req, res) => {
-//   res.render('top.ejs');
-// });
-
-// app.get('/index', (req, res) => {
-//   connection.query(
-//     'SELECT * FROM items',
-//     (error, results) => {
-//       res.render('index.ejs', {items: results});
-//     }
-//   );
-// });
-
-// app.get('/new', (req, res) => {
-//   res.render('new.ejs');
-// });
-
-// app.post('/create', (req, res) => {
-//   connection.query(
-//     'INSERT INTO items (name) VALUES (?)',
-//     [req.body.itemName],
-//     (error, results) => {
-//       res.redirect('/index');
-//     }
-//   );
-// });
-
-// app.post('/delete/:id', (req, res) => {
-//   connection.query(
-//     'DELETE FROM items WHERE id = ?',
-//     [req.params.id],
-//     (error, results) => {
-//       res.redirect('/index');
-//     }
-//   );
-// });
-
-// app.get('/edit/:id', (req, res) => {
-//   connection.query(
-//     'SELECT * FROM items WHERE id = ?',
-//     [req.params.id],
-//     (error, results) => {
-//       res.render('edit.ejs', {item: results[0]});
-//     }
-//   );
-// });
-
-// app.post('/update/:id', (req, res) => {
-//   connection.query(
-//     'UPDATE items SET name = ? WHERE id = ?',
-//     [req.body.itemName, req.params.id],
-//     (error, results) => {
-//       res.redirect('/index');
-//     }
-//   );
-// });
-
-// app.listen(2000);
-
 const express = require('express');
 const mysql = require('mysql');
+
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'list_app'
+    host: '127.0.0.1',
+    user: 'root',
+    password: '', //delete 
+    database: 'DeCartography'
+});
+
+//error handle
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
 });
 
 app.get('/', (req, res) => {
-  res.render('top.ejs');
-});
-
-// app.get('/index', (req, res) => {
-//   connection.query(
-//     'SELECT * FROM items',
-//       (error, results) => {
-//           res.render('index.ejs', { items: results });
-//     }
-//   );
-// });
-app.get('/index', (req, res) => {
   connection.query(
-    'SELECT * FROM items',
+    'SELECT * FROM addresses;',
     (error, results) => {
-      res.render('index.ejs', {items: results});
-    }
-  );
-    //   (error, results) => {
-    //     console.log(results);
-    //     res.render('index.ejs');
-    // }
-    // );
-});
-
-app.get('/new', (req, res) => {
-  res.render('new.ejs');
-});
-
-app.post('/create', (req, res) => {
-  connection.query(
-    'INSERT INTO items (name) VALUES (?)',
-    [req.body.itemName],
-    (error, results) => {
-      res.redirect('/index');
+      res.render('index.ejs', {content: results});
     }
   );
 });
 
-app.post('/delete/:id', (req, res) => {
+
+app.get('/work', (req, res) => {
   connection.query(
-    'DELETE FROM items WHERE id = ?',
-    [req.params.id],
+    'SELECT * FROM addresses;',
     (error, results) => {
-      res.redirect('/index');
+      res.render('work.ejs', {content: results});
     }
   );
 });
 
-app.get('/edit/:id', (req, res) => {
-  connection.query(
-    'SELECT * FROM items WHERE id = ?',
-    [req.params.id],
-    (error, results) => {
-      res.render('edit.ejs', {item: results[0]});
-    }
-  );
-});
 
+function joinTarget(target) {
+  return Array.isArray(target) ? target.join(',') : target;
+}
+
+//タグづけの更新作業のコードを以下に記述
 app.post('/update/:id', (req, res) => {
   // 選択されたメモを更新する処理を書いてください
   connection.query(
-    'UPDATE items SET name = ? WHERE id = ?',
-    [req.body.itemName, req.params.id],
+    'UPDATE adresses SET tag = ? WHERE id = ?',
+      [req.body.itemTag, req.params.id],
     (error, results) => {
-      res.redirect('/index');
+      res.redirect('/work');
     }
     );
-  // 以下の一覧画面へリダイレクトする処理を削除してください
 });
 
-app.listen(3003);
+
+app.listen(3001);
--- items.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO items(id,name) VALUES (1,'じゃがいも');
-INSERT INTO items(id,name) VALUES (2,'にんじん');
-INSERT INTO items(id,name) VALUES (3,'たまねぎ');
