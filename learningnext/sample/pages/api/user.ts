ba43373a3b5af4ee8ec8df3ea10643654dd27be8
tkgshn <takagishunsuke1129@gmail.com>
2022-10-28 21:30:43 -0400

    ここに表示する方法側から表示する方法がわからん

---
+++ learningnext/sample/pages/api/user.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+const { cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const serviceAccount = require('../../firebase-test-serviceAccount.json'); // 秘密鍵を取得
+const admin = require('firebase-admin');
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const COLLECTION_NAME = 'users';
+  //　初期化する
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: cert(serviceAccount),
+    });
+  }
+  const db = getFirestore();
+
+  const targetDoc = '5qzDOwBOar694KK0S6A7'; //書き換える
+  if (req.method === 'POST') {
+    const docRef = db.collection(COLLECTION_NAME).doc();
+    const insertData = {
+      datano: '1',
+      name: 'Symfo',
+      email: 'symfo@example.com',
+    };
+    docRef.set(insertData);
+  } else if (req.method === 'PATCH') {
+    const docRef = db.collection(COLLECTION_NAME).doc(targetDoc);
+    const updateData = {
+      datano: '1',
+      name: 'updateSynfo',
+      email: 'updateSynfo@example.com',
+    };
+    docRef.set(updateData);
+  } else if (req.method === 'GET') {
+    const doc = await db.collection(COLLECTION_NAME).doc(targetDoc).get();
+    console.log(doc);
+  } else if (req.method === 'DELETE') {
+    const doc = await db.collection(COLLECTION_NAME).doc(targetDoc).delete();
+  }
+  res.status(200);
+}
---
+++ learningnext/sample/pages/index.tsx
@@ -0,0 +1,63 @@
+import type { NextPage } from 'next';
+import axios from 'axios';
+// import doc from '../api/user';
+
+const Home: NextPage = () => {
+  const insertUser = async () => {
+    await axios.post('/api/user');
+  };
+  const updateUser = async () => {
+    await axios.patch('/api/user');
+  };
+  const getUser = async () => {
+    await axios.get('/api/user');
+  };
+  const deleteUser = async () => {
+    await axios.delete('/api/user');
+  };
+
+  return (
+
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+
+    <ul>
+      {/* {getUser.map(({user}:{user:any}) => (
+        <li key={user.datano}>
+          {user.name}
+          <br></br>
+          {user.email}
+        </li>
+      ))} */}
+    {/* const user = {getUser} */}
+    ここに情報を表示する方法がわからん
+    
+    </ul>
+
+
+
+
+      <button
+        className="mt-4 w-60 rounded-full bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-700"
+        onClick={() => insertUser()}>
+        Insert User
+      </button>
+      <button
+        className="mt-4 w-60 rounded-full bg-yellow-500 py-2 px-4 font-bold text-white hover:bg-yellow-700"
+        onClick={() => updateUser()}>
+        Update User
+      </button>
+      <button
+        className="mt-4 w-60 rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+        onClick={() => getUser()}>
+        Get User
+      </button>
+      <button
+        className="mt-4 w-60 rounded-full bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700"
+        onClick={() => deleteUser()}>
+        Delete User
+      </button>
+    </div>
+  );
+};
+
+export default Home;
